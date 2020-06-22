93c26759aa9641aecdaa0e5bef26d0a3482e159f
JamesPaynter <jpaynter@student.unimelb.edu.au>
2020-06-22 11:45:10 +1000

    splitting gravlens docs up

--- examples/gravlensnaturebasic.py
+++ examples/gravlensnaturebasic.py
@@ -1,4 +1,5 @@
-import numpy as np
+import sys, os
+import argparse
 
 from PyGRB.main.fitpulse import PulseFitter
 from PyGRB.backend.makemodels import create_model_from_key
@@ -6,26 +7,28 @@ from PyGRB.backend.makemodels import make_two_pulse_models
 
 
 
-def load_973(sampler = 'dynesty', nSamples = 100):
-    test = PulseFitter(973, times = (-2, 50),
-                datatype = 'discsc', nSamples = nSamples, sampler = sampler,
-                priors_pulse_start = -5, priors_pulse_end = 50,
-                priors_td_lo = 0,  priors_td_hi = 30, p_type ='docs')
+def load_3770(sampler = 'dynesty', nSamples = 100, **kwargs):
+    test = PulseFitter(3770, times = (-.1, 1),
+                datatype = 'tte', nSamples = nSamples, sampler = sampler,
+                priors_pulse_start = -.1, priors_pulse_end = 0.6,
+                priors_td_lo = 0,  priors_td_hi = 0.5, **kwargs)
     return test
 
 
-def evidence_for_973():
-    num_samples = [500]
+def evidence_for_3770():
+    num_samples = [2000]
     for samples in num_samples:
-        GRB = load_973(sampler=SAMPLER, nSamples=samples)
+        GRB = load_3770(sampler = SAMPLER, nSamples = samples)
+        GRB.offsets = [0, 4000, 8000, -3000]
+
         keys = ['FL', 'FF']
         model_dict = {}
         for key in keys:
             model_dict[key] = create_model_from_key(key)
         models = [model for key, model in model_dict.items()]
+
         for model in models:
             GRB.main_multi_channel(channels = [0, 1, 2, 3], model = model)
-
             lens_bounds = [(0.37, 0.42), (0.60, 1.8)]
             GRB.lens_calc(model = model, lens_bounds = lens_bounds)
         GRB.get_evidence_from_models(model_dict = model_dict)
@@ -33,4 +36,4 @@ def evidence_for_973():
 
 if __name__ == '__main__':
     SAMPLER = 'nestle'
-    evidence_for_973()
+    evidence_for_3770()
